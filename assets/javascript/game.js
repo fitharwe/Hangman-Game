ff4d81a66a808f4f6f590e9d7a00e4a2309e0a0c
fitharwe <jesterenwalker@gmail.com>
2018-03-28 21:10:05 -0400

    css

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+var words = [
+    "Saint Olaf",
+    "Miami",
+    "Sicily",
+    "Sophia",
+    "Dorothy",
+    "Blanche",
+    "Rose",
+]
+var wins = 0
+var losses = 0
+var guesses = 10
+var letters = []
+var anyKey = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+
+
